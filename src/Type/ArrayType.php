a29b23ee1735cd238c702f65a4fe21fa341cafdc
Marko Kunic <kunicmarko20@gmail.com>
2018-07-07 23:11:56 +0200

    fix case if Array is empty in parameters

--- src/Type/ArrayType.php
+++ src/Type/ArrayType.php
@@ -12,13 +12,17 @@ final class ArrayType implements TypeInterface
      */
     private $value;
 
-    public function __construct(array $value)
+    public function __construct(array $value = [])
     {
         $this->value = $value;
     }
 
     public function __invoke($identifier): string
     {
+        if (empty($this->value)) {
+            return sprintf('%s: []', $identifier);
+        }
+
         return sprintf('%s: ["%s"]', $identifier, implode('","', $this->value));
     }
 }
